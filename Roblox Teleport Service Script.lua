06fc645798c8f5de7e2fefff8bb7d4f76e12b4bf
CallInWar <70778242+CallInWar@users.noreply.github.com>
2021-01-03 17:10:50 -0600

    Update Roblox Teleport Service Script.lua

--- Roblox Teleport Service Script.lua
+++ Roblox Teleport Service Script.lua
@@ -1,6 +1,3 @@
 local TPS = game:GetService("TeleportService")
  
-TPS:Teleport(game id)
-
--- Put in StarterGUI
--- If you want to delete the script go to StarterGUI and find it then press delete.
+TPS:Teleport(Game ID)
